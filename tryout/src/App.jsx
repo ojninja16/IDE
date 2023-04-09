5909e847f53bd4b6d2602640768aadf21da3392b
Unic-X <armansinghkshatri69@gmail.com>
2023-04-09 16:59:09 +0530

    frontend commit

--- tryout/src/App.jsx
+++ tryout/src/App.jsx
@@ -15,11 +15,12 @@ const App = () => {
     <div className={` flex py-0 my-2 justify-between items-center flex-col`}>
     <h1 className={`font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] absolute top-0 left-10 py-0 my-1`}>IS IDE</h1>
     <header className="App-header">
-      <div className="px-10 flex flex-col mx-2 my-2 py-9 justify-between">
-      <CodeMirror
+      <div className="my-20 grid grid-cols-2 gap-2 grid-rows-3">
+        <div className='row-span-2 '>
+        <CodeMirror
             value={code}
-            height="400px"
-            width ="700px"
+            height="700px"
+            width ="900px"
             theme="dark"
             extensions={[javascript({ jsx: true })]}
             onChange={(editor, data, value) => {
@@ -27,14 +28,17 @@ const App = () => {
             }}
             className="flex flex-col "
           />
+          </div>
 
+          <div className='bg-white'> <input type="text" id="large-input" class="block w-full h-full text-start p-2.5"></input></div>
+          <div >Output</div>
           <div
             onClick={() => checkCode()}
-            className="border-2 p-2 bg-green-400 justify-center items-center text-white font-poppins font-semibold text-[1rem] rounded-md mt-5 pt-2 cursor-pointer px-0.75 rem py-1.5 rem  "
+            className="h-10 border-2 p-2 bg-green-400 justify-center items-center text-white font-poppins font-semibold text-[1rem] rounded-md pt-2 cursor-pointer px-0.75 rem py-1.5 rem  "
           >
             Submit Code
-          </div>
-          </div>
+          </div> 
+      </div>
           </header> 
           </div>
       
