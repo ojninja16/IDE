64ef1bd3768471d91f2020442af320b411c5cf3b
Arman <73340774+Unic-X@users.noreply.github.com>
2023-04-09 18:00:12 +0530

    login page

---
+++ tryout/src/components/Login.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import GitHubLogin from 'react-github-login';
+
+
+import "./styles.css";
+
+
+class App extends Component {
+
+  
+
+  render() {
+
+    const onSuccessGithub = (response) => {
+      console.log(response.code);
+    } 
+
+    return (
+      <div className="App" align="center">
+        <h1>LOGIN WITH GITHUB</h1>
+
+          {/*CLIENTID REDIRECTURI NOT CREATED YET*/}
+          <div className="input-container" >
+          <div className="form">
+            
+              <label><GitHubLogin clientId="eadc3759d300d268d2be"
+                  onSuccess={onSuccessGithub}
+                  buttonText="LOGIN WITH GITHUB"
+                  className="git-login"
+                  valid={true}
+                  redirectUri="http://samaj"
+          />   </label>
+            </div>
+        </div>
+
+          
+
+          <br />
+          <br />
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
